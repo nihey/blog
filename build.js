c148490a3f7f44914789ecd83e5b1e7903b92b5c
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-11-21 18:04:00 -0200

    Basic html building functions

--- build.js
+++ build.js
@@ -1,8 +1,24 @@
-var chokidar = require('chokidar');
+var fs = require('fs'),
+    path = require('path'),
+    $ = require('cheerio').load(fs.readFileSync('./index.html'));
+    chokidar = require('chokidar');
 
 var callback = function(file) {
-  // Build/Rebuild the project here
-  console.log('file:', file);
+  var html = '';
+  var extension = file.split('.');
+  if (extension[extension.length - 1] === 'js') {
+    delete require.cache[path.join(process.cwd(), file)];
+    var func = require('./' + file);
+    html += func();
+  } else {
+    html += fs.readFileSync(file);
+  }
+
+  $('.main-content').html(html);
+  var distPath = file.replace('content/', 'dist/').replace(/.js$/, '.html');
+  fs.writeFileSync(distPath, $.html());
+
+  console.log('built: ', file);
 };
 
 chokidar.watch('content/').on('add', callback).on('change', callback);
